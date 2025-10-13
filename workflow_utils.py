1c759b35f57e6f3a847db304d7eb6251c01bc012
forester3 <32355731+forester3@users.noreply.github.com>
2025-10-13 12:35:56 +0900

    support SDXL workflow

--- workflow_utils.py
+++ workflow_utils.py
@@ -8,12 +8,14 @@ def find_node_ids_from_connections(workflow_nodes_dict):
 
         if ntype == "KSampler":
             inputs = node.get("inputs", {})
-            for key, label in [("positive", "PositivePrompt_TextEncode"), ("negative", "NegativePrompt_TextEncode")]:
+            for key, label in [("positive", "PositivePrompt_TextEncode"),
+                               ("negative", "NegativePrompt_TextEncode")]:
                 ref = inputs.get(key)
                 if isinstance(ref, list) and len(ref) == 2:
                     ref_id = str(ref[0])
                     ref_node = node_by_id.get(ref_id)
-                    if ref_node and (ref_node.get("class_type") == "CLIPTextEncode" or ref_node.get("type") == "CLIPTextEncode"):
+                    if ref_node and (ref_node.get("class_type") in ["CLIPTextEncode", "CLIPTextEncodeSDXL"] or
+                                     ref_node.get("type") in ["CLIPTextEncode", "CLIPTextEncodeSDXL"]):
                         found[label] = ref_id
             found["KSampler"] = nid_str
 
